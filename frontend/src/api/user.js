c950fdff99a87c54c2ecdaa7adc21e248accfa12
Mikhail Zakharov <cashalotkzn@gmail.com>
2021-01-26 22:11:11 +0300

    Фикс

--- frontend/src/api/user.js
+++ frontend/src/api/user.js
@@ -4,7 +4,7 @@ export default {
   async getInfo(userName = '') {
     return Vue.prototype.$http({
       method: 'GET',
-      url: `/users/${userName}/`,
+      url: (userName) ? `/users/${userName}` : '/users',
     });
   },
 };
